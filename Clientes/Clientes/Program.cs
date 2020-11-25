12716dee9d61ed5b7c657a08927bf2698aa96a55
manucertalpuga <75039025+manucertalpuga@users.noreply.github.com>
2020-11-25 19:04:00 +0100

    Add files via upload

---
+++ Clientes/Clientes/Program.cs
@@ -0,0 +1,19 @@
+﻿using Clientes.Ui;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clientes
+{
+    using WForms = System.Windows.Forms;
+    static class Program
+    {
+        static void Main()
+        {
+            WForms.Application.Run(new MainWindowCtrl().View);
+
+        }
+    }
+}
