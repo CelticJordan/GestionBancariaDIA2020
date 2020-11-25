5008eacf23ab7c61cc4ffcb600fc631256f162ec
Ruben Solveira <66205167+rubensolveira@users.noreply.github.com>
2020-11-25 19:22:15 +0100

    Add files via upload

---
+++ Cuentas/CuentaVivienda.cs
@@ -0,0 +1,17 @@
+﻿namespace DIA_P2_banco.Cuentas
+{
+    public class CuentaVivienda : Cuenta
+    {
+        public CuentaVivienda(string ccc, Cliente cliente) : 
+            base(ccc, cliente)
+        {
+            base.Tipo = "Vivienda";
+            base.InteresMensual = 0.3f;
+        }
+        
+        public void AddDeposito(Deposito deposito)
+        {
+            this.Depositos.Add(deposito);
+        }
+    }
+}
