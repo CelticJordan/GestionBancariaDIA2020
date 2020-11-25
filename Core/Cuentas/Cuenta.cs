573fc6b11a6467d64bf6091be05ccbfc28aba2b1
Ruben Solveira <66205167+rubensolveira@users.noreply.github.com>
2020-11-25 19:35:40 +0100

    Add files via upload

---
+++ Core/Cuentas/Cuenta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DIA_P2_banco.Cuentas
+{
+    public abstract class Cuenta {
+        public class Deposito {
+            public string Concepto;
+            public DateTime DateTime;
+            public float Cantidad;
+
+            public Deposito(string concepto, float cantidad){
+                this.Concepto = concepto;
+                this.Cantidad = cantidad;
+                this.DateTime = DateTime.UtcNow;
+            }
+        }
+        public class Retirada {
+            public string Concepto;
+            public DateTime DateTime;
+            public float Cantidad;
+
+            public Retirada(string concepto, float cantidad) {
+                this.Concepto = concepto;
+                this.Cantidad = cantidad;
+                this.DateTime = DateTime.UtcNow;
+            }
+        }
+        public string CCC { get; private set; }
+        public string Tipo { get; protected set; }
+        public float Saldo { get; set; }
+        public List<Cliente> Titulares;
+        public DateTime FechaApertura { get; private set; }
+        public float InteresMensual { get; set; }
+        public List<Deposito> Depositos;
+        public List<Retirada> Retiradas;
+        
+        
+
+        protected Cuenta(string ccc, Cliente cliente)
+        {
+            this.CCC = ccc;
+            this.Titulares = new List<Cliente>();
+            this.Titulares.Add(cliente);
+            this.FechaApertura = DateTime.UtcNow;
+            this.Depositos = new List<Deposito>();
+            this.Retiradas = new List<Retirada>();
+        }
+    }
+}
---
+++ Core/Cuentas/CuentaAhorro.cs
@@ -0,0 +1,17 @@
+﻿namespace DIA_P2_banco.Cuentas
+{
+    public class CuentaAhorro : Cuenta
+    {
+        public CuentaAhorro(string ccc, Cliente cliente) : 
+            base(ccc, cliente)
+        {
+            base.Tipo = "Ahorro";
+            base.InteresMensual = 0.1f;
+        }
+        
+        public void AddDeposito(Deposito deposito)
+        {
+            this.Depositos.Add(deposito);
+        }
+    }
+}
