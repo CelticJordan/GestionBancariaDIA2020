e7f874a10df525630b60d085d8661585349729eb
ExecuteOrder66 <miguelariasperez@gmail.com>
2020-12-05 18:13:50 +0100

    Cuenta ToString

--- DIA_BANCO_V2/DIA_BANCO_V1/Core/Cuentas/Cuenta.cs
+++ DIA_BANCO_V2/DIA_BANCO_V1/Core/Cuentas/Cuenta.cs
@@ -15,6 +15,14 @@ namespace DIA_BANCO_V1
                 this.Cantidad = cantidad;
                 this.DateTime = dt;
             }
+            public override string ToString()
+            {
+                StringBuilder toret = new StringBuilder();
+                toret.AppendLine("\r\tConcepto: " + this.Concepto);
+                toret.AppendLine("\r\tFecha: " + this.Fecha);
+                toret.AppendLine("\r\tCantidad: " + this.Cantidad);
+                return toret.ToString();
+            }
         }
         public class Retirada {
             public string Concepto;
@@ -25,6 +33,14 @@ namespace DIA_BANCO_V1
                 this.Concepto = concepto;
                 this.Cantidad = cantidad;
                 this.DateTime = dt;
+            }
+            public override string ToString()
+            {
+                StringBuilder toret = new StringBuilder();
+                toret.AppendLine("\r\tConcepto: " + this.Concepto);
+                toret.AppendLine("\r\tFecha: " + this.Fecha);
+                toret.AppendLine("\r\tCantidad: " + this.Cantidad);
+                return toret.ToString();
             }
         }
         public string CCC { get; set; }
@@ -46,6 +62,20 @@ namespace DIA_BANCO_V1
             this.FechaApertura = DateTime.UtcNow;
             this.Depositos = new List<Deposito>();
             this.Retiradas = new List<Retirada>();
+        }
+        public override string ToString()
+        {
+            StringBuilder toret = new StringBuilder();
+            toret.AppendLine("\r\tCCC: " + this.CCC);
+            toret.AppendLine("\r\tTipo: " + this.Tipo);
+            toret.AppendLine("\r\tSaldo: " + this.Saldo);
+            for (int i = 0; i < this.Titulares.Count; i++)
+            {
+                toret.AppendLine("\r\tTitulares: " + this.Titulares[i].Nombre.ToString()); 
+            }
+            toret.AppendLine("\r\tFecha Apertural: " + this.FechaApertura);
+            toret.AppendLine("\r\tInteres: " + this.Interes);
+            return toret.ToString();
         }
     }
 }
