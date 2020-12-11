0e9b99e820483bd41d08531801bcc3b48ad824b7
Jorge Manuel Lozano Gómez <jmlgomez73@gmail.com>
2020-12-11 02:46:10 +0100

    mejor organizacion de ficheros

---
+++ DIA_BANCO_V1/Vista/Transferencia/ConsultarTransferenciaController.cs
@@ -0,0 +1,20 @@
+
+namespace DIA_BANCO_V1
+{
+    using WForms = System.Windows.Forms;
+
+    public class ConsultarTransferenciaController
+    {
+
+        public ConsultarTransferenciaController()
+        {
+
+            this.View = new ConsultarTransferencia();
+
+        }
+
+        public ConsultarTransferencia View { get; }
+        
+        
+    }
+}
